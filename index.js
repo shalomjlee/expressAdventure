e74892a76b8a041a123d0d02b1648090c33d6f4a
Shalom Lee <shalomjlee@gmail.com>
2022-01-03 09:20:03 -0600

    finished up warrior route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { response } = require("express");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3317
@@ -22,61 +23,68 @@ app.get("/archer/:monster", (request, response) => {
     const name = request.params["monster"]
     let special_message;
 
-    if(name=="zombie"){
-        special_message="Your swift arrows slay your foe!"
-    } else if (name=="slime") {
-        special_message = "Your arrows prove ineffective against your gelatinous foe, swallowing you whole"
-    } else if (name=="skeleton") {
-        special_message = "Your arrows prove no match for the skeleton's stony exterior."
-    } else {
-        special_message = "How did you get here?"
-    }
+    // if(name=="zombie"){
+    //     special_message="Your swift arrows slay your foe!"
+    // } else if (name=="slime") {
+    //     special_message = "Your arrows prove ineffective against your gelatinous foe, swallowing you whole"
+    // } else if (name=="skeleton") {
+    //     special_message = "Your arrows prove no match for the skeleton's stony exterior."
+    // } else {
+    //     special_message = "How did you get here?"
+    // }
     // response.render("archer", {monster:name})
     response.send(special_message)
 })
 
 
 app.get("/warrior", (request, response) => {
-    response.render("warrior")
+    const chooseFoe = "Chooose your Foe!"
+    const monsters = [
+        {
+            monster: "Zombie"
+        },
+        {
+            monster: "Slime"
+        },
+        {
+            monster: "Skeleton"
+        }
+    ]
+    response.render("warrior", {monsters: monsters, choose: chooseFoe})
 })
-app.get("/warrior/:monster", (request, response) => {
-    const name = request.params["monster"]
-    let special_message;
-
-    if(name=="zombie"){
-        special_message="Your blows prove ineffective as the Zombie claws its way towards you!"
-    } else if (name=="slime") {
-        special_message = "Your blows are absorbed by your gelatinous foe, and acid is corroding your sinews..."
-    } else if (name=="skeleton") {
-        special_message = "Your blows prove effective in smashing this underworldly scum!"
-    }else {
-        special_message = "How did you get here?"
-    }
 
 
-
-    // response.render("warrior",{monster:name})
-    response.send(special_message)
+app.get("/warrior/second", (request, response) => {
+    const chooseFoe = "Chooose your Foe!"
+    const monsters = [
+        {
+            monster: "Zombie"
+        },
+        {
+            monster: "Slime"
+        }
+    ]
+    response.render("warriorSecond", {monsters: monsters, choose: chooseFoe})
+    // response.send(special_message)
 })
-app.get("/warrior/:monster/:monster", (request, response) => {
-    const name = request.params["monster"]
-    let special_message;
-
-    if(name=="zombie"){
-        special_message="Your blows prove ineffective as the Zombie claws its way towards you!"
-    } else if (name=="slime") {
-        special_message = "Your blows are absorbed by your gelatinous foe, and acid is corroding your sinews..."
-    } else if (name=="skeleton") {
-        special_message = "Your blows prove effective in smashing this underworldly scum!"
-    }else {
-        special_message = "How did you get here?"
-    }
 
 
 
-    // response.render("warrior",{monster:name})
-    response.send(special_message)
+app.get("/warrior/third", (request, response) => {
+    const chooseFoe = "Chooose your Foe!"
+    const monsters = [
+        {
+            monster: "Zombie"
+        }
+
+    ]
+    response.render("warriorThird", {monsters: monsters, choose: chooseFoe})
 })
+
+
+
+
+
 
 
 app.get("/mage", (request, response) => {
@@ -98,6 +106,17 @@ app.get("/mage/:monster", (request, response) => {
     // response.render("mage",{monster:name})
     response.send(special_message)
 })
+
+
+
+app.get("/failure", (req, res) => {
+    res.render("failure")
+})
+
+app.get("/victory", (req, res) => {
+    res.render("victory")
+})
+
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`)
