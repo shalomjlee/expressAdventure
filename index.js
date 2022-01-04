a1f33fd62b9486fd108b299f23dbfcc9c99d65bd
Shalom Lee <shalomjlee@gmail.com>
2022-01-04 13:44:23 -0600

    added sessions

--- index.js
+++ index.js
@@ -1,40 +1,112 @@
 const { response } = require("express");
 const express = require("express");
+const req = require("express/lib/request");
 const app = express();
-const port = process.env.PORT || 3317
+const port = process.env.PORT || 8080
+var session = require("express-session");
+const { name } = require("ejs");
+const res = require("express/lib/response");
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json()) //to parse incoming requests with json payloads
+
+app.use(session({
+    secret: "random string",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}))
+
 
 app.set("view engine", "ejs")
 
+
 app.use(express.static( __dirname + "/public"))
 
-app.get("/", (request, response) => {
 
-    const charClass = ["Warrior", "Archer", "Mage"];
-    response.render("index", { user: charClass})
+
+
+const auth = (req, res, next) => {
+    if (req.session && req.session.username) {
+        next();
+    } else {
+        res.redirct("/?reason=invalidlogin")
+    }
+}
+
+
+app.get("/", (req, res) => {
+    let user = "";
+    let punctuation = "";
+    const url = req.url
+    
+    let invalid_login = false;
+    if (req.session && req.session.username) {
+        user = req.session.username;
+        punctuation = ",";
+    }
+    res.render("index", {my_user: user, punctuation: punctuation, invalid_login: invalid_login});
 });
+
+app.post('/signup', (req,res) => {
+
+    const valid_users = [
+        {"name": "sue", "password":"sue"},
+        {"name": "john", "password":"john"},
+        {"name": "sam", "password":"sam"},
+        {"name": "mary", "password":"mary"}
+    ]
+
+    const user = req.body.username;
+    const pass = req.body.password;
+
+    const found_User = valid_users.find(usr => {
+        return usr.name == user && usr.password == pass
+    })
+    
+    if (found_User) {
+        req.session.username = user;
+        res.redirect('/crossroads');
+    } else {
+        req.session.destroy(() => {
+            console.log("user reset");
+        })
+        res.redirect("/?reason=invalid_user");
+    }
+})
+
+app.get("/signout", (req,res) => {
+    req.session.destroy(() => {
+        res.end("You have been signed out")
+    })
+})
+app.get("/*", auth);
+
+app.get("/crossroads", (req, res) => {
+    if(req.session.username && req.session){
+        res.render("crossroads", {user: req.session.username})
+
+    } else {
+        req.session.destroy(()=> {
+        })
+        res.redirect("/")
+    }
+
+})
 
 
 app.get("/archer", (request, response) => {
     response.render("archer")
-
 })
 
-app.get("/archer/:monster", (request, response) => {
-    const name = request.params["monster"]
-    let special_message;
 
-    // if(name=="zombie"){
-    //     special_message="Your swift arrows slay your foe!"
-    // } else if (name=="slime") {
-    //     special_message = "Your arrows prove ineffective against your gelatinous foe, swallowing you whole"
-    // } else if (name=="skeleton") {
-    //     special_message = "Your arrows prove no match for the skeleton's stony exterior."
-    // } else {
-    //     special_message = "How did you get here?"
-    // }
-    // response.render("archer", {monster:name})
-    response.send(special_message)
-})
+
+// app.get("/archer/:monster", (request, response) => {
+//     const name = request.params["monster"]
+//     let special_message;
+
+//     response.send(special_message)
+// })
 
 
 app.get("/warrior", (request, response) => {
@@ -53,7 +125,6 @@ app.get("/warrior", (request, response) => {
     response.render("warrior", {monsters: monsters, choose: chooseFoe})
 })
 
-
 app.get("/warrior/second", (request, response) => {
     const chooseFoe = "Chooose your Foe!"
     const monsters = [
@@ -69,7 +140,6 @@ app.get("/warrior/second", (request, response) => {
 })
 
 
-
 app.get("/warrior/third", (request, response) => {
     const chooseFoe = "Chooose your Foe!"
     const monsters = [
@@ -80,11 +150,6 @@ app.get("/warrior/third", (request, response) => {
     ]
     response.render("warriorThird", {monsters: monsters, choose: chooseFoe})
 })
-
-
-
-
-
 
 
 app.get("/mage", (request, response) => {
